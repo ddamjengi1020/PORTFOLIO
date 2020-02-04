eb73da47025367b21728289cd918ee5f268d7860
ddamjengi <silluat11@gmail.com>
2020-02-05 01:10:15 +0900

    about modify

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -2,38 +2,51 @@ const headerContainer = document.getElementById("jsHeaderContainer");
 const headerContents = document.getElementById("jsHeaderContents");
 const li = headerContents.querySelectorAll("li");
 
-// switch 문으로 바꿀 것!
+const COLOR = "scrollColor";
+
 const headerColor = () => {
-  if (window.scrollY < 500) {
-    if ((li[0].className = "scrollColor")) {
-      li[0].classList.remove("scrollColor");
-    }
-  } else if (window.scrollY >= 500 && window.scrollY < 726) {
-    li[0].classList.add("scrollColor");
-    if ((li[1].className = "scrollColor")) {
-      li[1].classList.remove("scrollColor");
-    }
-  } else if (window.scrollY > 726 && window.scrollY < 900) {
-    li[0].classList.remove("scrollColor");
-    li[1].classList.add("scrollColor");
-    if ((li[2].className = "scrollColor")) {
-      li[2].classList.remove("scrollColor");
-    }
+  const SCROLLY = window.scrollY;
+  const a =
+    (SCROLLY < 500) * 1 +
+    (SCROLLY >= 500 && SCROLLY < 726) * 2 +
+    (SCROLLY > 726 && SCROLLY < 900) * 3;
+  switch (a) {
+    case 1:
+      if ((li[0].className = COLOR)) {
+        li[0].classList.remove(COLOR);
+      }
+      break;
+    case 2:
+      li[0].classList.add(COLOR);
+      if ((li[1].className = COLOR)) {
+        li[1].classList.remove(COLOR);
+      }
+      break;
+    case 3:
+      li[0].classList.remove(COLOR);
+      li[1].classList.add(COLOR);
+      if ((li[2].className = COLOR)) {
+        li[2].classList.remove(COLOR);
+      }
+      break;
+    default:
+      return;
   }
 };
 
 const handleWheel = () => {
-  if (window.scrollY > 490) {
-    headerContainer.style.animation = "scrollEvent 0.3s linear forwards";
-    scrollValue = window.scrollY;
+  const SCROLLY = window.scrollY;
+  if (SCROLLY > 0) {
+    headerContainer.style.animation = "scrollEvent 0.2s linear forwards";
+    scrollValue = SCROLLY;
     for (let i = 0; i < li.length; i++) {
-      li[i].style.animation = "scrollEventFont 0.3s linear forwards";
+      li[i].style.animation = "scrollEventFont 0.2s linear forwards";
     }
   } else {
-    headerContainer.style.animation = "scrollEventDefault 0.3s linear forwards";
-    scrollValue = window.scrollY;
+    headerContainer.style.animation = "scrollEventDefault 0.2s linear forwards";
+    scrollValue = SCROLLY;
     for (let i = 0; i < li.length; i++) {
-      li[i].style.animation = "scrollDefaultFont 0.3s linear forwards";
+      li[i].style.animation = "scrollDefaultFont 0.2s linear forwards";
     }
   }
   headerColor();
