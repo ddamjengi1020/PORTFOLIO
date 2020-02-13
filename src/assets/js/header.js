049d7b18df64d8b3261f28db800c6f866b559006
ddamjengi <silluat11@gmail.com>
2020-02-13 14:39:22 +0900

    follow floating menu

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -5,31 +5,48 @@ const li = headerContents.querySelectorAll("li");
 const COLOR = "scrollColor";
 
 const headerColor = () => {
-  const SCROLLY = window.scrollY;
-  const a =
-    (SCROLLY < 500) * 1 +
-    (SCROLLY >= 500 && SCROLLY < 726) * 2 +
-    (SCROLLY > 726 && SCROLLY < 900) * 3;
+  console.log(window.scrollY);
+  const SCROLLY = Math.ceil(window.scrollY);
+  const a = (SCROLLY > 300) * 1 + (SCROLLY <= 420) * 2;
+
   switch (a) {
     case 1:
+      for (let i = 0; i < li.length; i++) {
+        li[i].innerText = "▪";
+        li[i].style.color = "rgba(0, 0, 0, 0.637)";
+      }
       break;
     case 2:
-      break;
-    case 3:
+      li[0].innerText = "About";
+      li[1].innerText = "Skills";
+      li[2].innerText = "Project";
+      li[3].innerText = "Git";
+      for (let i = 0; i < li.length; i++) {
+        li[i].style.color = "white";
+      }
       break;
     default:
       return;
   }
 };
 
+//  following floating menu
+let num = 0;
 const handleWheel = e => {
-  if (e.deltaY > 0) {
-    headerContainer.style.animation = "scrollEvent 2s linear forwards";
-  } else if (e.deltaY === 0) {
-    headerContainer.style.animation = "";
-  } else if (e.deltaY < 0) {
-    headerContainer.style.animation = "scrollEventDefault 2s linear forwards";
+  const br = document.createElement("br");
+  const brAll = headerContents.querySelectorAll("br");
+  if (e.deltaY > 1.25) {
+    headerContents.append(br);
+  } else if (e.deltaY <= 1.25 && e.deltaY >= -1.25) {
+    for (let i = 0; i < brAll.length; i++) {
+      setTimeout(() => brAll[i].remove(), num);
+      num = num + 20;
+    }
+    num = 0;
+  } else if (e.deltaY < -1.25) {
+    headerContents.prepend(br);
   }
+  headerColor();
 };
 
 const init = () => {
